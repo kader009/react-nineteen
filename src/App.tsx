d73ff55c330e7970a7e4c41eae384728509fb602
kader009 <kadermollarasel000@gmail.com>
2024-10-14 17:46:00 +0600

    learn react new feature

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const App = () => {
+  const [get, setget] = useState()
+  return (
+    <div className="text-blue-500">
+      hellow
+    </div>
+  );
+};
+
+export default App;
