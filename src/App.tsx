41d67f9b8ee67d33ec81a84cfdd5016186ba7fd6
kader009 <kadermollarasel000@gmail.com>
2024-10-15 22:36:04 +0600

    usestate add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-
 const App = () => {
-  const [get, setget] = useState()
   return (
-    <div className="text-blue-500">
-      hellow
-    </div>
+    <>
+      <div className="text-blue-500">hellow</div>
+    </>
   );
 };
 
---
+++ src/pages/Switch.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function useTggle(initialValue = false){
+  const [state, setState] = useState(initialValue)
+}
+const Switch = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Switch;
