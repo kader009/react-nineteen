338df9161a3b71bbd6878008531ba609d62187b6
kader009 <kadermollarasel000@gmail.com>
2024-10-16 23:00:02 +0600

    learn profiler

--- src/pages/DataFetching.tsx
+++ src/pages/DataFetching.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Profiler, useCallback, useEffect, useState } from 'react';
 
 const DataFetching = () => {
   const [count, SetCount] = useState(0);
@@ -7,23 +8,38 @@ const DataFetching = () => {
     console.log('I am loggin..');
   }, []);
 
-  useCallback(() =>{},[])
+  useCallback(() => {}, []);
 
   const increment = useCallback(() => {
     SetCount(count + 1);
     SetCount(count + 1);
   }, [count]);
 
+
+  const onRenderCallback = (
+    id: any, 
+    phase: any,
+    actualDuration: any, 
+    baseDuration: any, 
+    startTime: any, 
+    commitTime: any, 
+  ) => {
+    console.log({ id, phase, actualDuration, baseDuration, startTime, commitTime });
+  };
+
+
   return (
-    <div className="text-center">
-      {count}
-      <button
-        className="bg-black px-2 py-1 text-white rounded"
-        onClick={increment}
-      >
-        Increment
-      </button>
-    </div>
+    <Profiler id="dataFetching" onRender={onRenderCallback}>
+      <div className="text-center">
+        {count}
+        <button
+          className="bg-black px-2 py-1 text-white rounded"
+          onClick={increment}
+        >
+          Increment
+        </button>
+      </div>
+    </Profiler>
   );
 };
 
